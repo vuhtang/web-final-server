c7115a365292fb1fd8e31e85d5f52a5aa9c7fa21
Ivan Varyuhin <varyuhin03@mail.ru>
2023-01-22 23:04:59 +0300

    microfix

--- src/main/java/com/vuhtang/lesha/LeshaApplication.java
+++ src/main/java/com/vuhtang/lesha/LeshaApplication.java
@@ -2,18 +2,11 @@ package com.vuhtang.lesha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
 public class LeshaApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeshaApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
